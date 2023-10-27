4ca90eae0c1a3070bfd8a9ca775d70280c2eb68e
Pascal-Hub <sequencepascal@gmail.com>
2023-10-27 09:04:57 -0700

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-""" Lockboxes interview coding challenge """
+"""
+    A function that determines if a number of lists of lists can be
+    opened by keys whose value is the number of the box
+"""
 
-from typing import List
+def canUnlockAll(boxes):
+    """This determines if the boxes (lists of lists) can be unlocked"""
+    position = 0
+    unlocked = {}
 
-
-def canUnlockAll(boxes: List[list]) -> bool:
-    """ checks if the all boxes can be unlocked
-
-    Args:
-        boxes (List[list]): List of boxes
-
-    Returns:
-        bool: True if all boxes can be unlocked else fals
-    """
-    unlocked_boxes = {}
-    try:
-        empty_index = boxes.index([])
-        boxes[empty_index] = [0]
-    except Exception:
-        pass
-    box_numbers = [x for x in range(len(boxes))]
-    # print(some)
-    unlocked_boxes[0] = 'unlocked'
-    for i in range(len(boxes) - 1):
-        for k in range(len(boxes[i])):
-            if boxes[i][k] in box_numbers:
-                unlocked_boxes[boxes[i][k]] = 'unlocked'
-
-    opened = unlocked_boxes.values()
-    if len(opened) != len(boxes):
-        return False
-    return True
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "now_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
